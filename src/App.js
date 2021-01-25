9846cc827fbff4d7553c077db08227bde3237230
Sayed Atif Ali <sayedali@amazon.com>
2021-01-24 17:07:29 -0800

    Added network call to fetch users dynamically

--- src/App.js
+++ src/App.js
@@ -15,7 +15,15 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({robots: robots});
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({robots: users})
+      });
+
+    // this.setState({robots: robots});
   }
 
   onSearchChange = (event) => {
@@ -27,14 +35,19 @@ class App extends React.Component {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
 
-    return (
-      <div className="tc">
-        <h1 className="f1"> RoboFriends </h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
-    );
-  }
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+          <div className="tc">
+            <h1 className="f1"> RoboFriends </h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots} />
+          </div>
+        );
+      }
+    }
+
 }
 
 export default App;
