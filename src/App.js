7abc54a95b9ace8cd3978b47b9876925b9267b57
Sayed Atif Ali <sayedali@amazon.com>
2021-01-24 16:38:11 -0800

    CSS background style changes and font updates.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import CardList from './CardList'
 import SearchBox from './SearchBox'
 import {robots} from './robots';
 import 'tachyons';
+import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+  }
+
+  componentDidMount() {
+    this.setState({robots: robots});
   }
 
   onSearchChange = (event) => {
@@ -21,11 +26,10 @@ class App extends React.Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
-    console.log(filteredRobots);
 
     return (
       <div className="tc">
-        <h1> RoboFriends </h1>
+        <h1 className="f1"> RoboFriends </h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots} />
       </div>
